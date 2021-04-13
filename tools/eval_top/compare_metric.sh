aacb366260d068471370acc31572c4b0a1edd204
chulutao <chulutao@baidu.com>
2021-04-13 03:14:20 +0000

    modity compare_metric.sh

--- tools/eval_top/compare_metric.sh
+++ tools/eval_top/compare_metric.sh
@@ -1,12 +1,24 @@
 # export CUDA_VISIBLE_DEVICES=1
 
-./build/seg_eval
-python deploy/bin2png.py
+#./build/seg_eval
+python3.7 bin2png.py --save_dir predict_cxx/
 
-echo "c++ metric"
-python deploy/eval_predict_imgs.py
+echo "*************************\n" | tee log.txt
+echo "*************************\n" | tee -a log.txt
+echo "******** c++:  *******\n" | tee -a log.txt
+echo "*************************\n" | tee -a log.txt
+echo "*************************\n" | tee -a log.txt
+python3.7 eval_predict_imgs.py --pred_dir predict_cxx/ | tee -a log.txt
 
 
-echo "python metric:"
-python val.py --config configs/fcn_hrnetw18_small_v1_humanseg_192x192_mini_supervisely.yml \
---model_path saved_model/fcn_hrnetw18_small_v1_humanseg_192x192_bs64_lr0.1_iter2w_horizontal_distort/best_model/model.pdparams
+python3.7 infer.py --config model/fcn_hrnetw18_small_v1_not_fix_shape_argmax_humanseg_192x192_bs64_lr0.1_iter2w_horizontal_distort/deploy.yaml \
+--file_path data/mini_supervisely/val.txt \
+--data_dir data/mini_supervisely \
+--save_dir predict_python/Images \
+--without_argmax
+echo "*************************\n" | tee -a log.txt
+echo "*************************\n" | tee -a log.txt
+echo "******** python:  *******\n" | tee -a log.txt
+echo "*************************\n" | tee -a log.txt
+echo "*************************\n" | tee -a log.txt
+python3.7 eval_predict_imgs.py --pred_dir predict_python/ | tee -a log.txt
